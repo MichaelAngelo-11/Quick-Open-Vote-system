e7b2738d11a118440d88541824605de0321947bb
Masalale <c.chomba@alustudent.com>
2025-11-16 16:26:02 +0200

    Remove old files and restructuring project

--- src/routes/elections.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createElection, getAllElections } = require("../controllers/electionsController");
-
-const router = express.Router();
-
-router.post("/create", createElection);
-router.get("/", getAllElections);
-
-module.exports = router;
--- src/routes/votes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { castVote, getVotesByElection } = require("../controllers/votesController");
-
-const router = express.Router();
-
-router.post("/cast", castVote);
-router.get("/:election_id", getVotesByElection);
-
-module.exports = router;
